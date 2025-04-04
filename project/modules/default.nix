830e57198dbc05a146f4593df53ead2a0b418d17
kauhsika <kaushika2003@gmail.com>
2025-04-04 22:45:47 +0530

    Create default.nix

---
+++ project/modules/default.nix
@@ -0,0 +1,11 @@
+{
+  base = import ./base;
+  editor = import ./editor;
+  impermanence = import ./impermanence;
+  proxy = import ./proxy;
+  k3svm = import ./k3svm;
+  mediastack = import ./mediastack;
+  #secrets = import ./secrets;
+  secureboot = import ./secureboot;
+  #selfhosted = import ./selfhosted;
+}
