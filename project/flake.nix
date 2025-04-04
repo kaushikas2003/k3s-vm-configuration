1b113a9e89c74554abe2084d480ea9aa249240c7
kauhsika <kaushika2003@gmail.com>
2025-04-04 23:15:56 +0530

    Create flake.nix

---
+++ project/flake.nix
@@ -0,0 +1,65 @@
+{
+  description = " NixOS Flake";
+  inputs = {
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    microvm = {
+      url = "github:astro/microvm.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    tailscale={
+      {
+  # Inside your module or host config
+  services.tailscale.enable = true;
+  networking.firewall.allowedUDPPorts = [ 41641 ];
+}
+
+    }
+  };
+  outputs = inputs @ {nixpkgs, ...}: let
+    systems = [
+      "x86_64-linux"
+      #"aarch64-linux" #Might add rpi4 later.
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+    nixosSystem = hostname:
+      nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [./profiles/${hostname}];
+      };
+  in {
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    overlays = import ./overlays {inherit inputs;};
+    nixosModules = import ./modules;
+    nixosConfigurations = {
+      citadel = nixosSystem "citadel";
+      pathfinder = nixosSystem "pathfinder";
+      singularity = nixosSystem "singularity";
+    };
+  };
+}
